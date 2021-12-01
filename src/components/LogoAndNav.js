385b6edf9aa2f08953fc5b26caeaa3fce1e9aff6
Rawa Aref <rawa_aref@hotmail.com>
2021-12-01 12:04:57 +0100

    add Logo

--- src/components/LogoAndNav.js
+++ src/components/LogoAndNav.js
@@ -1,20 +1,29 @@
 const LogoAndNav = () => {
 
+    // Attaching Logo
     const logoContainer = document.createElement('div');
-    logoContainer.innerHTML = '<h1> Logo Container</h1>';
+    logoContainer.className = 'logoContainer';
+    logoContainer.innerHTML = '<img src="./static/panagora-logo.svg" alt="Panagora Logo"/>';
 
+    // createing nav element
     const nav = document.createElement('nav');
+    nav.className = 'nav';
     nav.innerHTML = '<li>Nav Element</li>';
 
+
+    // creating container for dynamic content
     const appContainer = document.createElement('div');
     appContainer.setAttribute('id', 'app');
     appContainer.innerHTML = '<h1>App Container</h1>'
 
+
+    // Attaching elements to DOM
     document.body.appendChild(logoContainer);
     document.body.appendChild(nav);
     document.body.appendChild(appContainer);
 
 
+    return;
 }
 
 export default LogoAndNav
